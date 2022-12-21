5fe0732a1841019fad81c66d2997fd924eb162b3
ookami-kb <ookami.kb@gmail.com>
2022-12-21 11:00:11 +0100

    upd

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,77 +1,93 @@
-import {Checker, Result} from "../src/checker";
+import {Checker, Result} from '../src/checker'
 
 test('skips validation if excludeTitle matches', () => {
   const excludeTitle = new RegExp('Skip')
   const checker = new Checker({errorSize: 20, warningSize: 10, excludeTitle})
-  const result = checker.check({title: 'Skip PR size check', files: [{additions: 100, filename: '1.txt'}], labels: []})
+  const result = checker.check({
+    title: 'Skip PR size check',
+    files: [{additions: 100, filename: '1.txt'}],
+    labels: []
+  })
   expect(result).toBe(Result.ok)
-});
+})
 
 test('does validation if excludeTitle not matches', () => {
   const excludeTitle = new RegExp('NO_VALIDATION')
   const checker = new Checker({errorSize: 20, warningSize: 10, excludeTitle})
   const result = checker.check({
-    title: 'Skip PR size check', files: [{additions: 100, filename: '1.txt'}],
+    title: 'Skip PR size check',
+    files: [{additions: 100, filename: '1.txt'}],
     labels: []
   })
   expect(result).toBe(Result.error)
-});
+})
 
 test('does validation if excludeLabels not matches', () => {
   const excludeTitle = new RegExp('NO_VALIDATION')
   const excludeLabels = ['skip']
   const checker = new Checker({errorSize: 20, warningSize: 10, excludeTitle, excludeLabels})
   const result = checker.check({
-    title: 'Skip PR size check', files: [{additions: 100, filename: '1.txt'}],
+    title: 'Skip PR size check',
+    files: [{additions: 100, filename: '1.txt'}],
     labels: ['no-skip']
   })
   expect(result).toBe(Result.error)
-});
+})
 
 test('does validation if excludeTitle is undefined', () => {
   const checker = new Checker({errorSize: 20, warningSize: 10})
   const result = checker.check({
-    title: 'Skip PR size check', files: [{additions: 100, filename: '1.txt'}],
+    title: 'Skip PR size check',
+    files: [{additions: 100, filename: '1.txt'}],
     labels: []
   })
   expect(result).toBe(Result.error)
-});
+})
 
 test('returns warning', () => {
   const checker = new Checker({errorSize: 20, warningSize: 10})
   const result = checker.check({
     title: 'PR',
-    files: [{additions: 10, filename: '1.txt'}, {additions: 5, filename: '2.txt'}],
+    files: [
+      {additions: 10, filename: '1.txt'},
+      {additions: 5, filename: '2.txt'}
+    ],
     labels: []
   })
   expect(result).toBe(Result.warning)
-});
+})
 
 test('returns errors', () => {
   const checker = new Checker({errorSize: 20, warningSize: 10})
   const result = checker.check({
     title: 'PR',
-    files: [{additions: 10, filename: '1.txt'}, {additions: 15, filename: '2.txt'}],
+    files: [
+      {additions: 10, filename: '1.txt'},
+      {additions: 15, filename: '2.txt'}
+    ],
     labels: []
   })
   expect(result).toBe(Result.error)
-});
+})
 
 test('returns OK', () => {
   const checker = new Checker({errorSize: 100, warningSize: 50})
   const result = checker.check({
     title: 'PR',
-    files: [{additions: 10, filename: '1.txt'}, {additions: 15, filename: '2.txt'}],
+    files: [
+      {additions: 10, filename: '1.txt'},
+      {additions: 15, filename: '2.txt'}
+    ],
     labels: []
   })
   expect(result).toBe(Result.ok)
-});
+})
 
 test('skips files matching excludePath', () => {
   const checker = new Checker({
     errorSize: 40,
     warningSize: 30,
-    excludePaths: ['**/test/**', 'README.md', '**/resources/**/*.json'],
+    excludePaths: ['**/test/**', 'README.md', '**/resources/**/*.json']
   })
   const result = checker.check({
     title: 'PR',
@@ -80,12 +96,12 @@ test('skips files matching excludePath', () => {
       {additions: 15, filename: '2.txt'},
       {additions: 100, filename: 'some/test/file.txt'},
       {additions: 100, filename: 'README.md'},
-      {additions: 100, filename: 'resources/1.json'},
+      {additions: 100, filename: 'resources/1.json'}
     ],
     labels: []
   })
   expect(result).toBe(Result.ok)
-});
+})
 
 test('skips files matching excludeLabels', () => {
   const checker = new Checker({
@@ -100,9 +116,9 @@ test('skips files matching excludeLabels', () => {
       {additions: 15, filename: '2.txt'},
       {additions: 100, filename: 'some/test/file.txt'},
       {additions: 100, filename: 'README.md'},
-      {additions: 100, filename: 'resources/1.json'},
+      {additions: 100, filename: 'resources/1.json'}
     ],
     labels: ['skip']
   })
   expect(result).toBe(Result.ok)
-});
+})
